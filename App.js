e437deed64f6eb95feb4fa4a08a0b14aec79dfdf
Carlos Barauna <cbarauna@MacBook-Air-de-Carlos.local>
2019-05-30 23:12:16 -0300

    Projeto Mobile

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+import tictactoe from "./src/tictactoll";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    tictactoe.start();
+
+    this.state = {
+      board: tictactoe.board,
+      gameOver: tictactoe.gameover
+    };
+  }
+  makePlay(index) {
+    tictactoe.make_play(index);
+    this.setState({
+      board: tictactoe.board,
+      gameOver: tictactoe.gameover
+    });
+  }
+
+  isGameOver() {
+    if (this.state.gameOver) {
+      return <Text>Game Over</Text>;
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.board.map((value, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.piece}
+            onPress={() => {
+              this.makePlay(index);
+            }}
+          >
+            <Text style={styles.pieceText}>{value}</Text>
+          </TouchableOpacity>
+        ))}
+        {this.isGameOver()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  piece: {
+    width: Dimensions.get("window").width / 3,
+    height: Dimensions.get("window").width / 3,
+    backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 0.5,
+    borderColor: "#111"
+  },
+  pieceText: {
+    fontSize: 60
+  }
+});
